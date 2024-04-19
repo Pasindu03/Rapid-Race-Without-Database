e7de051f3f32897c070f7fad97bfb620a9179343
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2024-04-19 20:16:04 +0530

    Finished Form Controller

--- src/main/java/org/example/rapidracewithoutdatabase/controller/MainFormController.java
+++ src/main/java/org/example/rapidracewithoutdatabase/controller/MainFormController.java
@@ -6,10 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -106,6 +103,32 @@ public class MainFormController implements Initializable {
         TableColumnImage.setCellValueFactory(new PropertyValueFactory<>("image"));
     }
 
+    private boolean isNameValidate(String str) {return str.matches("^[a-zA-Z]+$");}
+    private boolean isBreedValidate(String str) {return str.matches("^[a-zA-Z]+$");}
+    private boolean isJockeyValidate(String str) {return str.matches("^[a-zA-Z]+$");}
+
+    private boolean isIdValidate(String str) {
+        return str.matches("^\\d{1,2}$");
+    }
+
+    private boolean isAgeValidate(String str) {
+        return str.matches("^(0*[5-9]|1[0-5])$");
+    }
+
+    private boolean isRaceParticipated(String str) {
+        return str.matches("^\\d{1,2}$");
+    }
+    private boolean isRaceWon(String racesWon, String racesParticipated) {
+        int won = Integer.parseInt(racesWon);
+        int participated = Integer.parseInt(racesParticipated);
+        if (won > participated) {
+            showAlert("Error", "Number of races won cannot exceed the number of races participated.");
+            return false;
+        }
+        return racesWon.matches("^\\d{1,2}$");
+    }
+    private boolean isGroupValidate(String str) {return str.matches("^[A-D]$");}
+
     public void SaveToFileWhenDataEntered(ActionEvent actionEvent) {
         List<Horse>HorseList=horseController.getHorses();
         try {
@@ -161,6 +184,261 @@ public class MainFormController implements Initializable {
         }
     }
 
+    public void AddHorseWhenEntered(ActionEvent actionEvent) {
+        if (IsRaceButtonIsOn) {
+            showAlert("Error", "Cannot add horse. Race has already started.");
+            return;
+        }
+
+        if (IdTextField.getText().isEmpty() || NameTextField.getText().isEmpty() || AgeTextField.getText().isEmpty() || BreedTextField.getText().isEmpty() || JockeyTextField.getText().isEmpty() || ParticipatedRacesCountTextField.getText().isEmpty() || WonRacesCountTextField.getText().isEmpty() || GroupTextField.getText().isEmpty()) {
+            showAlert("Error", "Please fill in all fields and select an image .");
+            return;
+        }
+
+        if (horseController.getHorses().size() >= 20) {
+            showAlert("Error", "Cannot add more horses. Maximum limit (20) reached.");
+            return;
+        }
+
+        String group = GroupTextField.getText().toUpperCase();
+
+        if (!group.matches("[A-D]")) {
+            showAlert("Error", "Invalid group specified. Please choose a group from A - D.");
+            return;
+        }
+
+        long groupCount = horseController.getHorses().stream().filter(horse -> horse.getGroup().equals(group)).count();
+        if (groupCount >= 5) {
+            showAlert("Error", "Cannot add more horses to Group " + group + ". Maximum limit (5) reached for this group.");
+            return;
+        }
+
+        // Proceed with horse addition
+        String id = IdTextField.getText();
+        String name = NameTextField.getText();
+        String age = AgeTextField.getText();
+        String breed = BreedTextField.getText();
+        String jockeyName = JockeyTextField.getText();
+        String racesParticipated = ParticipatedRacesCountTextField.getText();
+        String racesWon = WonRacesCountTextField.getText();
+        Image image = ViewHorseImage.getImage();
+
+        if (!isIdValidate(id)) {
+            showAlert("Error", "Invalid Horse ID format. It should contains integer only.");
+            return;
+        }
+
+        if(!isAgeValidate(age)){
+            showAlert("Error", "Invalid Horse age format. It should be a number between 5 and 15.");
+            return;
+        }
+
+        if (!isNameValidate(name)) {
+            showAlert("Error", "Invalid Horse name format. It should contains only alphabets.");
+            return;
+        }
+
+        if (!isBreedValidate(breed)) {
+            showAlert("Error", "Invalid Horse breed format. It should contains only alphabets.");
+            return;
+        }
+
+        if (!isJockeyValidate(jockeyName)) {
+            showAlert("Error", "Invalid Jockey name format. It should contains only alphabets.");
+            return;
+        }
+
+        if (!isRaceParticipated(racesParticipated)) {
+            showAlert("Error", "Invalid number of races participated. It should be a number between 1 and 100.");
+            return;
+        }
+        if (!isRaceWon(racesWon, racesParticipated)){
+            showAlert("Error", "Invalid number of races won. It should be a number between 1 and 100.");
+            return;
+        }
+        if (!isGroupValidate(group)) {
+            showAlert("Error", "Invalid group format. It should be a letter from A - D.");
+            return;
+        }
+
+        // Check if the ID is unique and does not Repeat
+        if (horseController.getHorses().stream().anyMatch(horse -> horse.getId() == Integer.parseInt(id))) {
+            showAlert("Error", "ID already exists. Please select  a different ID.");
+            return;
+        }
+
+        int intID = Integer.parseInt(id);
+        int intAge = Integer.parseInt(age);
+        int intRacesParticipated = Integer.parseInt(racesParticipated);
+        int intRacesWon = Integer.parseInt(racesWon);
+
+        Horse newHorse = new Horse(intID, name, intAge, breed, jockeyName, intRacesParticipated, intRacesWon, group, image);
+        horseController.addHorse(newHorse);
+        showAlert("", "Horse added successfully.");
+        ViewHorsesTableView();
+        ClearTextFieldsMethod();
+    }
+
+    private void ClearTextFieldsMethod() {
+        IdTextField.clear();
+        NameTextField.clear();
+        AgeTextField.clear();
+        BreedTextField.clear();
+        JockeyTextField.clear();
+        ParticipatedRacesCountTextField.clear();
+        WonRacesCountTextField.clear();
+        GroupTextField.clear();
+        ViewHorseImage.setImage(null);
+    }
+
+    public void UpdateHorseWhenSelected(ActionEvent actionEvent) {
+        if (IsRaceButtonIsOn) {
+            showAlert("Error", "Cannot update horse. Race has already started.");
+            return;
+        }
+
+        Horse selectedHorse = ViewHorsesTable.getSelectionModel().getSelectedItem();
+
+        if (selectedHorse != null) {
+            String id = IdTextField.getText();
+            String name = NameTextField.getText();
+            String age = AgeTextField.getText();
+            String breed = BreedTextField.getText();
+            String jockeyName = JockeyTextField.getText();
+            String racesParticipated = ParticipatedRacesCountTextField.getText();
+            String racesWon = WonRacesCountTextField.getText();
+            String group = GroupTextField.getText();
+            Image image = ViewHorseImage.getImage();
+
+            if (!isIdValidate(id)) {
+                showAlert("Error", "Invalid Horse ID format. It should contains integer only.");
+                return;
+            }
+
+            if(!isAgeValidate(age)){
+                showAlert("Error", "Invalid Horse age format. It should be a number between 5 and 15.");
+                return;
+            }
+
+            if (!isNameValidate(name)) {
+                showAlert("Error", "Invalid Horse name format. It should contains only alphabets.");
+                return;
+            }
+
+            if (!isBreedValidate(breed)) {
+                showAlert("Error", "Invalid Horse breed format. It should contains only alphabets.");
+                return;
+            }
+
+            if (!isJockeyValidate(jockeyName)) {
+                showAlert("Error", "Invalid Jockey name format. It should contains only alphabets.");
+                return;
+            }
+
+            if (!isRaceParticipated(racesParticipated)) {
+                showAlert("Error", "Invalid number of races participated. It should be a number between 1 and 100.");
+                return;
+            }
+            if (!isRaceWon(racesWon, racesParticipated)){
+                showAlert("Error", "Invalid number of races won. It should be a number between 1 and 100.");
+                return;
+            }
+            if (!isGroupValidate(group)) {
+                showAlert("Error", "Invalid group format. It should be a letter from A - D.");
+                return;
+            }
+
+            int intID = Integer.parseInt(id);
+            int intAge = Integer.parseInt(age);
+            int intRacesParticipated = Integer.parseInt(racesParticipated);
+            int intRacesWon = Integer.parseInt(racesWon);
+
+            Horse updatedHorse = new Horse(intID, name, intAge, breed, jockeyName, intRacesParticipated, intRacesWon, group, image);
+
+            horseController.updateHorse(updatedHorse);
+
+            showAlert("Success", "Horse details updated successfully.");
+
+            ViewHorsesTableView();
+            ClearTextFieldsMethod();
+        } else {
+            showAlert("Error", "Please select a horse to update from the table.");
+        }
+    }
+
+    public void DeleteHorseWhenSelected(ActionEvent actionEvent) {
+        if (IsRaceButtonIsOn) {
+            showAlert("Error", "Cannot delete horse. Race has already started.");
+            return;
+        }
+
+        Horse selectedHorse = ViewHorsesTable.getSelectionModel().getSelectedItem();
+
+        if (selectedHorse == null) {
+            showAlert("Error", "Please select a  horse to be deleted from the table .");
+            return;
+        }
+
+        //Fill in the text fields with the information corresponding to the chosen horse
+        IdTextField.setText(String.valueOf(selectedHorse.getId()));
+        NameTextField.setText(selectedHorse.getName());
+        AgeTextField.setText(String.valueOf(selectedHorse.getAge()));
+        BreedTextField.setText(selectedHorse.getBreed());
+        JockeyTextField.setText(selectedHorse.getJockeyName());
+        ParticipatedRacesCountTextField.setText(String.valueOf(selectedHorse.getRacesParticipated()));
+        WonRacesCountTextField.setText(String.valueOf(selectedHorse.getRacesWon()));
+        GroupTextField.setText(selectedHorse.getGroup());
+
+        horseController.deleteHorse(selectedHorse);
+        showAlert("Success", "Horse deleted successfully.");
+        ViewHorsesTableView();
+        ClearTextFieldsMethod();
+    }
+    private void ViewHorsesTableView() {
+        ViewHorsesTable.getColumns().clear();
+
+        ViewHorsesTable.getItems().clear();
+
+        horseController.sortHorsesById();
+
+        TableColumnImage.setCellFactory(column -> {
+            return new TableCell< Horse, Image>() {
+                private final ImageView imageView = new ImageView();
+
+                @Override
+                protected void updateItem(Image image, boolean empty) {
+                    super.updateItem(image, empty);
+
+                    if (image == null || empty) {
+                        setGraphic(null);
+                    } else {
+                        imageView.setImage(image);
+                        imageView.setFitWidth(70);
+                        imageView.setFitHeight(70);
+                        setGraphic(imageView);
+                    }
+                }
+            };
+        });
+
+        //  Appending the necessary columns to the table view
+        ViewHorsesTable.getColumns().addAll(TableColumnID, TableColumnName, TableColumnAge, TableColumnBreed, TableColumnJockey, TableColumnRacesParticipatedCount, TableColumnRacesWonCount, TableColumnGroup, TableColumnImage);
+
+        // Set horse data directly to the table view
+        ViewHorsesTable.getItems().addAll(horseController.getHorses());
+    }
+
+    public void ClearTextFields(ActionEvent actionEvent) {
+        IdTextField.clear();
+        NameTextField.clear();
+        AgeTextField.clear();
+        BreedTextField.clear();
+        JockeyTextField.clear();
+        ParticipatedRacesCountTextField.clear();
+        WonRacesCountTextField.clear();
+        GroupTextField.clear();
+        ViewHorseImage.setImage(null);
+    }
     public void AddImageToTheHorseClass(ActionEvent actionEvent) {
         ImageButtonClicked.setOnAction(event -> {
             FileChooser fileChooser = new FileChooser();
